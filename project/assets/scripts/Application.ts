9c7e8aaeffc757eb85b9fa48bcadb9a0ea7e912b
ming <venus-ming@outlook.com>
2024-01-16 00:23:30 +0800

    初始化工程

---
+++ project/assets/scripts/Application.ts
@@ -0,0 +1,13 @@
+import ccclass = cc._decorator.ccclass;
+
+@ccclass
+export default class Application extends cc.Component {
+
+    protected onLoad() {
+
+    }
+
+    protected start() {
+
+    }
+}
